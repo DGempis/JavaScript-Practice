1d1adfe1da0369c9e1fcceb074fd64eb51255e26
Danzel Gempis <danzelgempis@gmail.com>
2022-07-14 13:15:54 -0700

    Day01 changes

--- Day01/index.js
+++ Day01/index.js
@@ -10,3 +10,15 @@ firstButton.addEventListener("click", function(){
     headerOne.textContent = "This Button's Event Listener Also Works!"
     headerOne.style.color = 'red';
 })
+
+// This is a single line comment
+
+/* This is a 
+multi line comment */
+
+const favoriteFood = "pizza"
+const name = "Danzel"
+
+console.log(favoriteFood);
+console.log(`My favorite food is ${favoriteFood}.`);
+console.log(`My name is ${name}.`);
