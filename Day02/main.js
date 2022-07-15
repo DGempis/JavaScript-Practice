f93549d432eb5855c47bc26cc25160ab95531765
Danzel Gempis <danzelgempis@gmail.com>
2022-07-15 09:23:50 -0700

    Day02

---
+++ Day02/main.js
@@ -0,0 +1,19 @@
+let firstHeader = document.querySelector(".header-ele")
+let container = document.getElementById("root")
+
+
+
+/* Day 2 Daily Challenge:
+In your js-practice repository:
+Create a new branch named day-2
+Using the .length property, console log the number of letters in “Supercalifragilisticexpialidocious”
+Console log any number
+Console log a BigInt
+Hint: Use typeof to confirm your number and BigInt values
+Add, commit, and push to your day-2 branch */
+
+const newWord = "Supercalifragilisticexpialidocious";
+
+console.log(newWord);
+console.log(newWord.length); // 34
+
